57586771b57dd9728013129e2756a7e199292095
Abir Majumdar <83433840+abirismyname@users.noreply.github.com>
2023-10-13 20:55:01 +0000

    Updating script

--- .devcontainer/on-setup.sh
+++ .devcontainer/on-setup.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-echo Starting Terraform init
-terraform init -upgrade
+end=$((SECONDS+10))
+
+while [ $SECONDS -lt $end ]; do
+  if command -v terraform &> /dev/null
+  then
+    echo "Terraform found"
+    echo "Running: terraform init -upgrade"
+    terraform init -upgrade
+    exit 0
+  fi
+  sleep 1
+done
+
+echo "Terraform could not be found after 10 seconds"
+exit 1
